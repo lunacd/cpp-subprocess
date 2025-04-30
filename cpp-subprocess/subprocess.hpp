27ac26ca4be64213820e220ae8a5b7dea08125c4
Haowen Liu <lhw@lunacd.com>
2025-04-29 20:58:09 -0400

    Add string() to buffer

--- cpp-subprocess/subprocess.hpp
+++ cpp-subprocess/subprocess.hpp
@@ -939,6 +939,7 @@ public:
   Buffer() {}
   explicit Buffer(size_t cap) { buf.resize(cap); }
   void add_cap(size_t cap) { buf.resize(cap); }
+  std::string string() const { return std::string(buf.begin(), buf.end()); }
 
 #if 0
   Buffer(const Buffer& other):
